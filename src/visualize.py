b58120d166feb75d492b3c1d2395a6bd727f54ac
Maxine Tamas <mtamas25@cmc.edu>
2023-04-26 11:04:37 -0700

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	new file:   output.png 	new file:   reduced.country 	new file:   reduced.lang 	new file:   run_maps.sh 	modified:   src/reduce.py 	modified:   src/visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -2,6 +2,9 @@
 
 # command line args
 import argparse
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_path',required=True)
 parser.add_argument('--key',required=True)
@@ -22,7 +25,26 @@ if args.percent:
     for k in counts[args.key]:
         counts[args.key][k] /= counts['_all'][k]
 
-# print the count values
+# print the count values (edited for bar graph)
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
-for k,v in items:
-    print(k,':',v)
+# get the top 10 items
+top_items = items[:10]
+
+# create lists for the x and y values
+x = []
+y = []
+for item in top_items:
+    x.append(item[0])
+    y.append(item[1])
+
+# create a bar graph using plt.bar()
+plt.bar(x, y)
+
+# set the labels for the axes
+plt.xlabel('Count')
+plt.ylabel('Input')
+plt.title("Input x Count")
+
+# show the plot
+plt.savefig('output.png')
+plt.show()
